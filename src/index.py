e823becc0fcac1e75cff65af03ed286efebd5194
Jason Goldstein <jason@betheshoe.com>
2016-07-15 19:06:00 -0400

    Combine javascript

--- src/index.py
+++ src/index.py
@@ -1,6 +1,5 @@
 from beagle.decorators import action
-from beagle.commands import Page, Copy
-
+from beagle.commands import Page, Copy, Concat
 
 @action
 def pages():
@@ -11,3 +10,8 @@ def assets():
     things = ["micromarkdown.js", "underscore-min.js", "app.css", "app.js", "problems"]
     copies = [Copy(**{"infile": thing}) for thing in things]
     return copies
+
+@action
+def js():
+    return Concat(filenames=["micromarkdown.js", "underscore-min.js", "app.js"],
+        destination="app.min.js")
