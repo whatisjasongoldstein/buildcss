048c91bb99de6f1375e2922841e678ed74cbdb6b
Jason Goldstein <jason@betheshoe.com>
2016-07-17 21:02:08 -0400

    Cooking with gas

--- src/index.py
+++ src/index.py
@@ -1,9 +1,30 @@
+import os
 from beagle.decorators import action
 from beagle.commands import Page, Copy, Concat
 
+PROBLEMS_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "problems")
+
 @action
 def pages():
-    return Page(template="index.html", output="index.html", context={})
+
+    # Get the slugs from the list of files in the problems folder
+    slugs = set([filename.split(".")[0] for filename in os.listdir(PROBLEMS_DIR)])
+    actions = []
+
+    # For each slug, render an html page, and copy the example
+    for slug in slugs:
+        if slug == "intro":
+            filename = "index.html"
+        else:
+            filename = "%s.html" % slug
+        page = Page(template="index.html", output=filename, context={
+            "iframe_src": "problems/%s.html" % slug,
+            "text": "problems/%s.md" % slug,
+        })
+        iframe = Copy(infile="problems/%s.html" % slug)
+        actions.append(page)
+        actions.append(iframe)
+    return actions
 
 @action
 def assets():
@@ -13,5 +34,5 @@ def assets():
 
 @action
 def js():
-    return Concat(filenames=["micromarkdown.js", "underscore-min.js", "app.js"],
+    return Concat(filenames=["underscore-min.js", "app.js"],
         destination="app.min.js")
